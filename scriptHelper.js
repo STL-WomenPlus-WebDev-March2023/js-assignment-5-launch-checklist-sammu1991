431650ccda1d0f20acef34c7fccb8d8fe998ef74
Your Name <you@example.com>
2023-07-12 18:28:43 -0500

    changes

--- scriptHelper.js
+++ scriptHelper.js
@@ -4,9 +4,7 @@ require('isomorphic-fetch');
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
    let missionTarget = document.getElementById('missionTarget');
-   missionTarget.innerHTML = `
-  
-                 <h2>Mission Destination</h2>
+   missionTarget.innerHTML = ` <h2>Mission Destination</h2>
                  <ol>
                     <li>Name: ${name}</li>
                      <li>Diameter: ${diameter}</li>
@@ -14,8 +12,7 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
                      <li>Distance from Earth: ${distance}</li>
                      <li>Number of Moons: ${moons}</li>
                 </ol>
-                <img src= "${imageUrl}">
-                    `;
+                <img src= "${imageUrl}">`;
 
 }
 
